95f6366e0e2959e57eba20129f7adb89b7e4940d
ryygeorge <ryangeorge1205@gmail.com>
2025-01-15 12:33:13 +0530

    Add files via upload

---
+++ investment.py
@@ -0,0 +1,4 @@
+invest = float(input("Enter the amount of the investment: "))
+return_amount = float(input("Enter the amount of the return: "))
+ret = (return_amount - invest)/invest
+print("The Return on investment is", ret)
